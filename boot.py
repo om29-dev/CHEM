f02f46222ee9f59bef0a0433acda832b604d4447
om29-dev <ombharambe901@pccoepune.org>
2024-10-22 08:42:16 +0530

    definining functions

--- boot.py
+++ boot.py
@@ -1,5 +1,7 @@
 #Cleaning Memory
 import gc
+import machine # type: ignore
+import time
 gc.collect()
 
 #Clearing Wifi Activity
@@ -10,17 +12,28 @@ ap = network.WLAN(network.AP_IF)
 ap.active(False)
 
 #Creating a Wifi Access Point
-ap.active(True)
-ap.config(ssid="Your Plant", key="password")
+def ap(a,b):
+    ap.active(True)
+    ap.config(ssid=a, key=b)
 
-#Connecting to my network
-sta.active(True)
-sta.connect('vivo1904','password')
+#Connecting to a network
+def sta(a,b):
+    sta.active(True)
+    sta.connect(a,b)
+
+# Getting the analog pin
+adc = machine.ADC(0)
+
+# Function to read soil moisture
+def moisture():
+    value = adc.read()
+    # Convert analog value to percentage
+    percent = 100 - ((value / 1023.0) * 100)
+    return percent
 
 #Blinking LED
-from machine import Pin # type: ignore
-from time import sleep
-led = Pin(16, Pin.OUT)
-led.off()
-sleep(1)
-led.on()
+def led(a):
+    led = machine.Pin(16, machine.Pin.OUT)
+    led.off()
+    time.sleep(a)
+    led.on()
---
+++ main.py
@@ -0,0 +1,7 @@
+import boot
+from time import sleep
+
+boot.ap('Your Plant','password')
+boot.sta('vivo1904','password')
+
+boot.led(1)

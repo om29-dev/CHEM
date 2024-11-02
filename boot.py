7cded5cf26b2e396cbea568b7228215496af9fa0
om29-dev <ombharambe901@gmail.com>
2024-11-02 11:13:51 +0530

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,11 +1,9 @@
 import gc
-import network # type: ignore
+from wifi import ap, sta
 
 #Cleaning Memory
 gc.collect()
 
 #Clearing Wifi Activity
-sta = network.WLAN(network.STA_IF)
 sta.active(False)
-ap = network.WLAN(network.AP_IF)
 ap.active(False)

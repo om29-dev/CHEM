68065bcd201cfb92de6b0f3356a3de12c0ae3568
om29-dev <ombharambe901@gmail.com>
2024-11-02 11:20:44 +0530

    Update led.py

--- led.py
+++ led.py
@@ -2,8 +2,9 @@ from machine import Pin # type: ignore
 from time import sleep_ms as delay
 
 #Blinking LED
-def blink(a):
+def blink(a,b):
     led = Pin(16, Pin.OUT)
-    led.off()
-    delay(a)
-    led.on()
+    for i in range(0,b):
+        led.off()
+        delay(a)
+        led.on()

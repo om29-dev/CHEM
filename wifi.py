1f48c2d517827ab3190fecebf90503a7481482a9
om29-dev <ombharambe901@gmail.com>
2024-11-03 19:03:47 +0530

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -3,6 +3,9 @@ import network # type: ignore
 ap = network.WLAN(network.AP_IF)
 sta = network.WLAN(network.STA_IF)
 
+x = "Wifi Hotspot"
+y = "Connected to Wifi"
+
 #Scanning nearby Wifi Networks
 def scan():
     sta.active(True)
@@ -14,8 +17,17 @@ def scan():
 def hotspot(a,b):
     ap.active(True)
     ap.config(ssid=a, key=b)
+    if ap.active():
+        ip=ap.ifconfig()[0]
+        print(x, "Created Successfully with IP Address:", ip)
+    else:
+        print("Unable to create", x)
 
 #Connecting to a Wifi Network
 def connect(a,b):
     sta.active(True)
     sta.connect(ssid=a, key=b)
+    if sta.isconnected():
+        print(y, "Sucessfully")
+    else:
+        print("Couldn't", y)

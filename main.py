1c0372b382a2731ea0b73a76a35e633c995464be
om29-dev <ombharambe901@gmail.com>
2024-11-04 09:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import wifi
+import server
 
 wifi.hotspot('Your Plant', 'password')
 wifi.connect('vivo1904', 'password')
+
+def start_server():
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0]
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+    print('Listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('Client connected from', addr)
+        server.handle(cl)
+
+start_server

caef7a383d5cb7eafc1924ac6b470c5d2e3f1d2a
om29-dev <ombharambe901@gmail.com>
2024-11-04 19:34:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import wifi
 import telegram as tg
+from time import sleep as delay
+import moisture
 
 wifi.hotspot('Your Plant', 'password')
 wifi.connect('vivo1904', 'password')
 
-def main():
-    offset = None
-    while True:
-        updates = tg.update(offset)
-        for item in updates['result']:
-            offset = item['update_id'] + 1
-            chat_id = item['message']['chat']['id']
-            text = item['message'].get('text')
-
-            if text == '/start':
-                send_message(chat_id, "Hello, I'm active. Use /status to know about me")
-
-        time.sleep(1)
-
-if __name__ == '__main__':
-    main()
+offset = None
+while True:
+    updates = tg.update(offset)
+    for item in updates['result']:
+        offset = item['update_id'] + 1
+        chat_id = item['message']['chat']['id']
+        text = item['message'].get('text')
+        if text == '/start':
+            message="Hello, I'm active. Use /status to know about me"
+        elif text == '/status':
+            message="The moisture in your plant is "+moisture.percent()+"%"
+        tg.send(chat_id, message)
+    delay(1)

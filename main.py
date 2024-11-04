4a7bdb8f15e615efc9e99f9fca73b8d0b55d6bdd
om29-dev <ombharambe901@gmail.com>
2024-11-04 19:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,3 @@ from time import sleep as delay
 
 wifi.hotspot('Your Plant', 'password')
 wifi.connect('vivo1904', 'password')
-
-def start_server():
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0]
-    s = socket.socket()
-    s.bind(addr)
-    s.listen(1)
-    print('Listening on', addr)
-    cl, addr = s.accept()
-    print('Client connected from', addr)
-    for i in range(0,2):
-        server.handle(cl)
-    while True:
-        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        cl.send(moisture.percent())
-        cl.close()
-        delay(1)
-
-start_server()

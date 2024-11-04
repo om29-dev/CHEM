14f8d8fd9bd9d33832c283df9c96c24531735f99
om29-dev <ombharambe901@gmail.com>
2024-11-04 19:21:25 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,1 +1,15 @@
-.
+import requests as req
+from time import sleep as delay
+import json
+
+TOKEN = 'YOUR_BOT_TOKEN'
+URL = f'https://api.telegram.org/bot{TOKEN}'
+
+def update(offset=None):
+    params = {'timeout': 100, 'offset': offset}
+    response = req.get(f'{URL}/getUpdates', params=params)
+    return response.json()
+
+def send(chat_id, text):
+    params = {'chat_id': chat_id, 'text': text}
+    req.post(f'{URL}/sendMessage', params=params)

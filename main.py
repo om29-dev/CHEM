882732fda2907fc024bc11607207c69255e002e7
om29-dev <ombharambe901@pccoepune.org>
2024-11-09 09:29:58 +0530

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import moisture
 
 wifi.hotspot('Your Plant', 'password')
 wifi.connect('vivo1904', 'password')
+a=moisture.percent()
 
 offset = None
 while True:
@@ -12,10 +13,14 @@ while True:
     for item in updates['result']:
         offset = item['update_id'] + 1
         chat_id = item['message']['chat']['id']
+        if a<20:
+            message="Your Plant needs to be watered"
         text = item['message'].get('text')
         if text == '/start':
             message="Hello, I'm active. Use /status to know about me"
         elif text == '/status':
-            message="The moisture in your plant is "+moisture.percent()+"%"
+            message="The moisture in your plant is "+a+"%"
+        else:
+            message="Invalid command"
         tg.send(chat_id, message)
     delay(1)
